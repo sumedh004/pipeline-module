36500886b12598c77932f353eb1b150772ec2ab3
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2022-06-09 17:17:02 +0530

    Update sns.tf

--- modules/terrapipeline/sns.tf
+++ modules/terrapipeline/sns.tf
@@ -6,7 +6,7 @@ resource "aws_sns_topic" "manual_approval" {
 resource "aws_sns_topic_subscription" "email" {
   topic_arn              = aws_sns_topic.manual_approval.arn
   protocol               = "email"
-  endpoint               = "sumedhvartak@outlook.com"
+  endpoint               = "ENTER YOUR EMAIL"
   #endpoint_auto_confirms = true
 
 
