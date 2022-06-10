7476f5ff9e675b80c60cdfe41cc8c94a57df23c7
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2022-06-10 11:51:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 provider "aws" {
-region = var.main_region
-}
-variable "github_token"{
-type=string
-description="Enter github token"
+  region = var.main_region
 }
 
 
 
 module "pipeline" {
-source = "./modules/terrapipeline"
-github_token=var.github_token
+  source                = "./modules/terrapipeline"
+  github_token          = var.github_token
+  repository_owner      = ""
+  repository_name       = ""
+  repository_branch     = ""
+  artifacts_bucket_name = ""
+  endpoint_email        = ""
+
+
 }
