b7af50a52e81c9c0e8d45de971eb96e8536cd4f7
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2022-06-10 11:55:16 +0530

    Update main.tf

--- modules/terrapipeline/main.tf
+++ modules/terrapipeline/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+    }
+
+    random = {
+      source = "hashicorp/random"
+    }
+
+  }
+}
+
 provider "aws" {
   region = var.region
 }
@@ -5,8 +18,4 @@ provider "aws" {
 provider "github" {
   token = var.github_token
   owner = var.repository_owner
-}
-
-provider "random" {
-
 }
